d00c200f4463d52a0e246f89ff99addcab0d48d3
mordor737 <procomtech737@gmail.com>
2019-11-10 16:41:01 +0530

    Apache kafka L2 Assignment

---
+++ Hands On 1/KafkaProducer/src/main/java/com/kafka/Producer.java
@@ -0,0 +1,27 @@
+package com.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Producer {
+public static void main(String args[]){
+    Properties properties=new Properties();
+    properties.put("bootstrap.servers","localhost:9092");
+    properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+    properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+    KafkaProducer kafkaProducer =new KafkaProducer(properties);
+    try{
+        for(int i=0;i<100;i++){
+            System.out.println(i);
+            kafkaProducer.send(new ProducerRecord("wipro", Integer.toString(i),"Test Message - "+i));
+        }
+    }catch(Exception e){
+        e.printStackTrace();
+    }finally{
+        kafkaProducer.close();
+    }
+}
+}
